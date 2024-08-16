267ef10c93209db1a1c2284b9dd22ab6dae2d0ff
Arsal-Naeem <arsalnaeem1@gmail.com>
2024-08-16 16:46:42 +0500

    fixed product grid

--- src/Data/ProductData.jsx
+++ src/Data/ProductData.jsx
@@ -53,6 +53,24 @@ const productData = [
     productName: "Slush Puppie Slushie Strawberry 250ml",
     productPrice: "2.99",
   },
+  {
+    productImage:
+      "https://skybluewholesale.com/content/images/thumbs/0018357_starbucks-frappuccino-cookies-cream-uk-8x250ml_260.png",
+    productName: "Starbucks Frappuccino Cookies & Cream (UK) 8x250ml",
+    productPrice: "29.99",
+  },
+  {
+    productImage:
+      "https://skybluewholesale.com/content/images/thumbs/0018363_samyang-pop-corn-snack-67g-1ct_550.png",
+    productName: "Samyang Pop Corn Snack 67g 1ct",
+    productPrice: "1.79",
+  },
+  {
+    productImage:
+      "https://skybluewholesale.com/content/images/thumbs/0018350_fanta-fruit-twist-pop-can-24x330ml_260.png",
+    productName: "Fanta Fruit Twist Pop Can 24x330ML",
+    productPrice: "31.79",
+  },
  
 ];
 
