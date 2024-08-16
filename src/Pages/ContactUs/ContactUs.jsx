3349742c137ed134fab72db556c58cb45a8618ad
Arsal-Naeem <arsalnaeem1@gmail.com>
2024-08-17 04:37:28 +0500

    added loader on contact

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout/Layout";
 import ContactUsGrid from "../../Components/ContactUsGrid/ContactUsGrid";
 import ContactForm from "../../Components/ContactForm/ContactForm";
 import Map from "../../Components/Map/Map";
 
 const ContactUs = () => {
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <Layout pageTitle="Contact Us">
+    <Layout pageTitle="Contact Us" isLoaded={isLoaded}>
       <ContactUsGrid />
       <ContactForm />
       <Map />
