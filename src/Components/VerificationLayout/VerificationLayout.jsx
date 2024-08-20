11716f39400674e89ac0d576eee95167da99d5ea
Arsal Naeem <arsalnaeem1@gmail.com>
2024-08-20 18:43:37 +0500

    added Loader on Verification Layout

--- src/Components/VerificationLayout/VerificationLayout.jsx
+++ src/Components/VerificationLayout/VerificationLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./VerificationLayout.css"
+import Loader from "../Loader/Loader";
 
-const VerifcationLayout = ({ children }) => {
+const VerifcationLayout = ({ children, isLoaded }) => {
   return (
-    <div className="verification-container">
+    <div className="verification-container" >
+        <Loader isActive={isLoaded} />
       {children}
     </div>
   );
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import LoginForm from "../../Components/loginform/loginform";
 import VerifcationLayout from "../../Components/VerificationLayout/VerificationLayout";
 
 const Login = () => {
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Replace with actual data fetching logic
+        await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate loading time
+        setIsLoaded(false); // Set to true once data is loaded
+      } catch (error) {
+        console.error("Failed to load data:", error);
+        setIsLoaded(false); // Handle loading failure if necessary
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <VerifcationLayout>
+    <VerifcationLayout isLoaded={isLoaded} > 
       <LoginForm />
     </VerifcationLayout>
   );
