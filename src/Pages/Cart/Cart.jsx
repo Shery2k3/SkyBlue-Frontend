300773a33a967b9b95cb99b313ace6ff28aad0c8
Arsal Naeem <arsalnaeem1@gmail.com>
2024-08-21 18:50:29 +0500

    added responsiveness to cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Layout from '../../Components/Layout/Layout';
-import CartItemGrid from '../../Components/CartItemGrid/CartItemGrid';
+import { useEffect, useState } from "react";
+import Layout from "../../Components/Layout/Layout";
+import CartItemGrid from "../../Components/CartItemGrid/CartItemGrid";
 
 const Cart = () => {
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setisLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <Layout pageTitle="Cart">
+    <Layout pageTitle="Cart" isLoading={isLoading}>
       <CartItemGrid />
     </Layout>
-  )
-}
+  );
+};
 
 export default Cart;
